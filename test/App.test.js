052d9193c0f18dbaef7f83e77d725be32eabeb81
Simon Garmendia <sgarmendia@gmail.com>
2018-09-05 12:42:28 +0200

    First rendering test passed

---
+++ test/App.test.js
@@ -0,0 +1,12 @@
+import App from '../src/components/App';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe('App', () => {
+  test('matches snapshot', () => {
+    const wrapper = shallow(<App />)
+
+    expect(wrapper.find('h1').text()).toBe('Welcome to Light React Boilerplate')
+    expect(wrapper).toMatchSnapshot
+  })
+})
